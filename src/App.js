b644f7a0efa79e974fd682f5b8e784c691398992
aishucb <121301185+aishucb@users.noreply.github.com>
2023-09-24 09:49:09 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter and Routes
-import Home from './home'; // Make sure the path is accurate
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './home';
 import WriteData from './WriteForm';
 import ReadData from './ReadForm';
-
 
 function App() {
   return (
     <Router>
-    <Routes> {/* Use Routes instead of Switch */}
-      <Route path="/" element={<Home />} />
-      <Route path="/write" element={<WriteData />} /> {/* Use element prop */}
-      <Route path="/read" element={<ReadData />} /> {/* Use element prop */}
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/write" element={<WriteData />} />
+        <Route path="/read" element={<ReadData />} />
+      </Routes>
+    </Router>
   );
 }
 
